b45030ca35ab7f9e4734591d11cea452b682f38c
kevinstephen221012 <rryyaann160218@gmail.com>
2026-07-19 15:21:07 +0530

    a

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ exponent=int(input("enter your exponent: "))
 result=1
 for i in range(exponent):
     result=result*n
-    print(f"{n} raised to the power {exponent} is:",result)
+print(f"{n} raised to the power {exponent} is:",result)
