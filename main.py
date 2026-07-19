276574bc0da07502be62617680ec9318350f397d
kevinstephen221012 <rryyaann160218@gmail.com>
2026-07-19 14:00:43 +0530

    a

---
+++ main.py
@@ -0,0 +1,6 @@
+n=int(input("enter your number: "))
+exponent=int(input("enter your exponent: "))
+result=1
+for i in range(exponent):
+    result=result*n
+    print(f"{n} raised to the power {exponent} is:",result)
